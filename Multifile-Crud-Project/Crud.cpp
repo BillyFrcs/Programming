f8ed9efb6afdd48a965027f9d9a6331522903df8
Billy Franscois <BillyFranscois@outlook.com>
2021-01-14 19:45:04 +0900

    Commit

---
+++ Multifile-Crud-Project/Crud.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    system("pause > 0");
+    cin.get();
+    return 0;
+}
