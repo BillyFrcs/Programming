2754433a6cca1d71a90a2f5edfc12227baa7fd29
Billy Franscois <BillyFranscois@outlook.com>
2021-02-04 19:28:09 +0900

    Update

--- build/CMakeFiles/Cmake.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Cmake.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ProgramCpp
-# Build directory: C:/ProgramCpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
