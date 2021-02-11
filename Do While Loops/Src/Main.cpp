5204ddb1ebf7d4fc160765b026fd847b613a312b
Billy Franscois <BillyFranscois@outlook.com>
2021-02-11 19:03:03 +0900

    Add

---
+++ Do While Loops/Src/Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void validationPinProgram()
+{
+     int userPin = 1234, Pin, errorCounter = 0;
+
+     do
+     {
+          cout << "Enter PIN: ";
+          cin >> Pin;
+
+          if (Pin != userPin)
+               errorCounter++;
+
+     } while (errorCounter < 3 && Pin != userPin);
+
+     if (errorCounter < 3)
+          cout << "Loading...";
+
+     else
+          cout << "Blocked...";
+}
+
+int main()
+{
+     //Pin validation program
+     validationPinProgram();
+
+     system("pause > 0");
+}
