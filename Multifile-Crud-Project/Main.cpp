43a27d79e5021b36b3ee55b4da677da807c5b689
BillyFranscois <billykolibonso19@gmail.com>
2021-02-14 19:44:33 +0900

    Update

--- Multifile-Crud-Project/Main.cpp
+++ Multifile-Crud-Project/Main.cpp
@@ -6,36 +6,8 @@
 //CRUD ==> Create Read Update And Delete
 using namespace std;
 
-/*/Prototype
-//Get option
-int getOption ();
-
-//Check database 
-void checkDataBase (fstream &data);
-
-//Write data mahasiswa
-void writeDataMahasiswa (fstream &data, int Position, Mahasiswa &inputMahasiswa);
-
-//Get data size
-int getDataSize  (fstream &data);
-
-//Read data mahasiswa
-Mahasiswa readDataMahasiswa (fstream &data, int Position);
-
-//Add data mahasiswa
-void addDataMahasiswa (fstream &data);
-
-//Display or show data mahasiswa 
-void displayDataMahasiswa (fstream &data);
-
-//Change or update data mahasiswa
-void changeDataMahasiswa (fstream &data);
-
-//Delete data mahasiswa 
-void deleteDataMahasiswa (fstream& data);*/
-
-//How to run this program on terminal
-//g++ Main.cpp Crud.cpp -o main
+//How to run this program on terminal (Windows)
+//g++ Main.cpp Crud.cpp -o Main
 
 int main()
 {
