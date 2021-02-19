e32a3790962872da5214766755d3c6d3cd10560d
BillyFranscois <billykolibonso19@gmail.com>
2021-02-19 20:54:26 +0900

    Add

--- BMI Calculator/Source/tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-
-               cout << "Overweight \
---
+++ Simple ATM Application/Source/Main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+void showMenu()
+{
+     cout << "*********Menu*********" << endl;
+     cout << "1. Check Balance" << endl;
+     cout << "2. Deposit" << endl;
+     cout << "3. Withdraw" << endl;
+     cout << "4. Exit" << endl;
+     cout << "**********************" << endl;
+
+     cout << endl;
+}
+
+void chooseOption()
+{
+     int option;
+     double balance = 500;
+
+     //Check balance, deposit, withdraw, show menu
+     do
+     {
+          cout << "Choose option: ";
+          cin >> option;
+
+          switch (option)
+          {
+          case 1:
+               cout << "Balance is: " << balance << " $" << endl;
+               break;
+
+          case 2:
+               cout << "Deposit amount: ";
+               double depositAmount;
+               cin >> depositAmount;
+
+               balance += depositAmount;
+               break;
+
+          case 3:
+               cout << "Withdraw amount: ";
+               double withdrawAmount;
+               cin >> withdrawAmount;
+
+               if (withdrawAmount <= balance)
+                    balance -= withdrawAmount;
+               else
+                    cout << "Not enough money" << endl;
+          }
+     } while (option != 4);
+}
+
+int main()
+{
+
+     showMenu();
+
+     chooseOption();
+
+     return 0;
+}
