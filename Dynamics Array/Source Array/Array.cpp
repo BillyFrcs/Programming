962d2a96d72e610a2099955782b490dfcd470f26
Billy Franscois <BillyFranscois@outlook.com>
2021-02-21 22:52:55 +0900

    Add

---
+++ Dynamics Array/Source Array/Array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+     int size;
+
+     cout << "Size: ";
+     cin >> size;
+
+     int *myArr = new int[size];
+
+     for (int i = 0; i < size; i++)
+     {
+          cout << "Array index[" << i << "]" << endl;
+          cin >> myArr[i];
+     }
+
+     for (int i = 0; i < size; i++)
+     {
+          //Method 1
+          cout << myArr[i] << " ";
+          //Method 2
+          //cout << *(myArr + i) << " ";
+     }
+
+     delete[]myArr;
+     myArr = NULL;
+
+     return 0;
+}
