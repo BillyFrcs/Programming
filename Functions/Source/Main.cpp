150a75c05c9c0607f3fe3f850dc64db8c3092816
BillyFranscois <billykolibonso19@gmail.com>
2021-02-22 19:50:51 +0900

    New Update

--- Functions/Source/Main.cpp
+++ Functions/Source/Main.cpp
@@ -14,6 +14,38 @@ int testFunctionPow(int num)
      return pow(num, 2);
 }
 
+//The scope of things
+string transportation = "Plane";
+
+double dataDouble = 15.80;
+
+void favoriteTransport(string favTransport)
+{
+
+     string Transportation = favTransport;
+     cout << "Fav transportation is" << favTransport << "\n";
+}
+
+void enterCode(int passcode)
+{
+
+     string secretKnowledge = "https://content.codecademy.com/courses/regex/onyourexcitingjourneylearningtocodeyouwillfindthis.gif";
+
+     cout << "Enter the passcode to access the link: ";
+     cin >> passcode;
+
+     if (passcode == 1908)
+     {
+
+          cout << "Here you go " << secretKnowledge << "\n";
+     }
+     else
+     {
+
+          cout << "Sorry, incorrect!\n";
+     }
+}
+
 int main()
 {
 
@@ -22,4 +54,16 @@ int main()
      cout << endl;
 
      cout << testFunctionPow(5) << endl;
+
+     cout << endl;
+
+     cout << transportation << endl;
+
+     favoriteTransport("Motor");
+
+     cout << endl;
+
+     enterCode(1908);
+
+     return 0;
 }
