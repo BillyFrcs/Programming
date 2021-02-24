bdca50c0ffa27d76af02f06eef7bd279a1b68fa3
BillyFranscois <billykolibonso19@gmail.com>
2021-02-24 11:50:51 +0900

    Update

---
+++ Array/Array Loops/Source Array/Array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+
+     string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Ferary"};
+
+     for (int i = 0;i < 5;i++)
+     {
+          cout << cars[i] << "\n";
+     }
+
+     return 0;
+}
