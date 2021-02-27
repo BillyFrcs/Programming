88386811489c9333bd78d0d14de86aad0a97bc90
BillyFranscois <billykolibonso19@gmail.com>
2021-02-27 15:46:21 +0900

    Update

--- Array/Array Loops/Source Array/Array.cpp
+++ Array/Array Loops/Source Array/Array.cpp
@@ -8,10 +8,22 @@ int main()
 
      string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Ferary"};
 
-     for (int i = 0;i < 5;i++)
+     for (int i = 0; i < 5; i++)
      {
           cout << cars[i] << "\n";
      }
+
+     cout << endl;
+
+     int a[8] = {};
+     int x = 3;
+
+     for (int i = 0; i < 6; i++)
+     {
+          a[i] = ++x;
+     }
+
+     cout << a[4] + a[5] * a[6] << endl;
 
      return 0;
 }
