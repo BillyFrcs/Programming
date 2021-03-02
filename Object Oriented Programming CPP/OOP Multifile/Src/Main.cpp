3bf85fbc5077ece3753df33e49a2488301a93353
Billy Franscois <BillyFranscois@outlook.com>
2021-03-02 13:20:36 +0900

    New

--- Object Oriented Programming CPP/OOP Multifile/Src/Main.cpp
+++ Object Oriented Programming CPP/OOP Multifile/Src/Main.cpp
@@ -7,5 +7,8 @@ int main(int argc, char const *argv[])
 {
     Song mySong;
 
-    
+    mySong.addTitle("Closer");
+    cout << mySong.getTitle(); //Use this for declare object from function data type
+
+    return 0;
 }
