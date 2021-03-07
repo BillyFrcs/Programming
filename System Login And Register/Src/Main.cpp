71aa2d857c6c47acee903b1577a7e52a2a34ba9b
Billy Franscois <69786531+BillyFrcs@users.noreply.github.com>
2021-03-07 18:58:41 +0900

    New Changes System Login

--- System Login And Register/Src/Main.cpp
+++ System Login And Register/Src/Main.cpp
@@ -4,29 +4,8 @@
 
 using namespace std;
 
-bool userLoggedIn()
-{
-     string username, password, un, pw;
-
-     //Input from user
-     cout << "Username: ";
-     cin >> username;
-
-     cout << "Password: ";
-     cin >> password;
-
-     //File user
-     ifstream read("data\\" + username + ".txt");
-     getline(read, un);
-     getline(read, pw);
-
-     //Comparison
-     if (un == username && pw == password)
-          return true;
-
-     else
-          return false;
-}
+//Prototype
+bool userLoggedIn();
 
 int main()
 {
@@ -40,16 +19,16 @@ int main()
      {
           string username, password;
 
-          cout << "Select username: ";
+          cout << "Add username: ";
           cin >> username;
 
-          cout << "Select password: ";
+          cout << "Add password: ";
           cin >> password;
 
           //File user
           ofstream file;
 
-          file.open("data\\" + username + ".txt");
+          file.open(username + ".txt"); 
           file << username << endl
                << password;
           file.close();
@@ -72,4 +51,28 @@ int main()
                return 1;
           }
      }
+}
+
+bool userLoggedIn()
+{
+     string username, password, un, pw;
+
+     //Input from user
+     cout << "Login Username: ";
+     cin >> username;
+
+     cout << "Login Password: ";
+     cin >> password;
+
+     //File user
+     ifstream read(username + ".txt"); 
+     getline(read, un);
+     getline(read, pw);
+
+     //Comparison
+     if (un == username && pw == password)
+          return true;
+
+     else
+          return false;
 }
