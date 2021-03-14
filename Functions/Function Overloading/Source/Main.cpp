53d94c3981f5f8b486e2ace4eea59985958af1c6
Billy Franscois <69786531+BillyFrcs@users.noreply.github.com>
2021-03-14 12:02:08 +0900

    Update Function Overloading

--- Functions/Function Overloading/Source/Main.cpp
+++ Functions/Function Overloading/Source/Main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int multiply(int x, int z);
+
 long sumLong(long a, long b);
 
 double sumDouble(double a, double b);
@@ -10,11 +12,19 @@ float sumFloat(float a, float b, float c);
 
 int main()
 {
+     int multiplyNum = multiply(5, 5);
+     cout << "Result int: " <<  multiplyNum << endl;
+
      cout << sumLong(5, 10) << endl;
      cout << sumDouble(9.5, 3.4) << endl;
      cout << sumFloat(6.5, 3.2, 4.6) << endl;
 
      return 0;
+}
+
+int multiply(int x, int z)
+{
+     return x * z;
 }
 
 long sumLong(long a, long b)
