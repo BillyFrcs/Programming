aba58029c0c8edd5d6cb179661b90b1570ef70e1
Billy Franscois <69786531+BillyFrcs@users.noreply.github.com>
2021-03-15 20:15:57 +0900

    Update Assignments

--- Assignments Program/Assignment 01/Src/Main.cpp
+++ Assignments Program/Assignment 01/Src/Main.cpp
@@ -9,41 +9,62 @@ using namespace std;
 //Global variable
 string name, TTL, asal, NIM;
 
+//Input biodata
 void myBiodata()
 {
-     cout << "Nama: ";
+     cout << "Nama : ";
      getline(cin, name);
 
-     cout << "NIM: ";
+     cout << "NIM  : ";
      getline(cin, NIM);
 
-     cout << "TTL: ";
+     cout << "TTL  : ";
      getline(cin, TTL);
 
-     cout << "Asal: ";
+     cout << "Asal : ";
      getline(cin, asal);
 }
 
+//Output biodata
 void displayMyBiodata()
 {
-     system("Color 1"); //Change color console
      system("cls"); //This for clear console
-     
-     cout << "==============================" << endl;
+
+     cout << "==================================" << endl;
+     cout << "BIODATA SAYA" << endl;
      cout << "Nama : " << name << endl;
      cout << "NIM  : " << NIM << endl;
      cout << "TTL  : " << TTL << endl;
      cout << "Asal : " << asal << endl;
-     cout << "===============================" << endl;
-     cout << "-------------------------------" << endl;
+     cout << "==================================" << endl;
+     cout << "----------------------------------" << endl;
 }
 
 int main()
 {
-     //Called function 
+     //Called function
      myBiodata();
 
      displayMyBiodata();
+
+     cout << endl;
+
+     int n1, n2, n3, n4, Nilai;
+     n1 = 80;
+     n2 = 50;
+     n3 = 60;
+     n4 = 80;
+
+     Nilai = (n1 + n2 + n3 + n4) / 4;
+
+     if (Nilai < 50)
+     {
+          cout << "Anda tidak lulus. Nilai anda " << Nilai << endl;
+     }
+     else
+     {
+          cout << "Anda LULUS dengan nilai " << Nilai << endl;
+     }
 
      return 0;
 }
