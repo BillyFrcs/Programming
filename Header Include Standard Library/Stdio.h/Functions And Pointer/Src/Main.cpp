52ff68c840e385dc2cefdcc01c43e71b48018a99
BillyFrcs <69786531+BillyFrcs@users.noreply.github.com>
2021-03-17 23:14:26 +0900

    Add Pointer And Functions

--- Header Include Standard Library/Stdio.h/Functions And Pointer/Src/Main.cpp
+++ Header Include Standard Library/Stdio.h/Functions And Pointer/Src/Main.cpp
@@ -2,16 +2,40 @@
 
 using namespace std;
 
+//Function display
+void display()
+{
+     printf("Yo");
+}
+
+//Function return
+int getNum()
+{
+     int x = 8;
+     return x;
+}
+
 int main()
 {
-
+     //Pointer
      int b = 19;
 
      int *p;
-     p = &b;
+     int **p2;
 
-     printf("%d", b);
-     printf("%d", p);
+     p = &b;
+     p2 = &p;
+
+     printf("%d \n", *p2);
+     printf("%d \n \n", &b);
+
+     display();
+
+     int showNum = getNum();
+
+     printf("\n");
+
+     printf("%d \n", showNum);
 
      return 0;
 }
