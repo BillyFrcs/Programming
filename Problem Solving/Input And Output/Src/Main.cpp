0ea89796c74bda5392639bb238f5657c53123976
Billy Franscois <69786531+BillyFrcs@users.noreply.github.com>
2021-03-20 14:07:00 +0900

    Add Problem Solving

---
+++ Problem Solving/Input And Output/Src/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+     //Addition
+     int n, n2, n3;
+
+     cin >> n >> n2 >> n3;
+
+     cout << n + n2 + n3 << endl;
+
+     return 0;
+}
