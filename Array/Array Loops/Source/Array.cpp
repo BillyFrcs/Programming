62008775cb690f1ec7cec3550758d5d23bd1a8d0
BillyFrcs <69786531+BillyFrcs@users.noreply.github.com>
2021-03-20 21:46:49 +0900

    Update Array Loops And Add Basics Array

--- Array/Array Loops/Source/Array.cpp
+++ Array/Array Loops/Source/Array.cpp
@@ -1,51 +1,73 @@
-#include <iostream>
 #include <array>
+#include <iostream>
 
 using namespace std;
 
-int main()
-{
+void listArr() {
+  string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Ferary"};
 
-     string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Ferary"};
+  for (int i = 0; i < 5; i++) {
+    cout << cars[i] << "\n";
+  }
 
-     for (int i = 0; i < 5; i++)
-     {
-          cout << cars[i] << "\n";
-     }
+  cout << endl;
 
-     cout << endl;
+  int a[8] = {};
+  int x = 3;
 
-     int a[8] = {};
-     int x = 3;
+  for (int i = 0; i < 6; i++) {
+    a[i] = ++x;
+  }
 
-     for (int i = 0; i < 6; i++)
-     {
-          a[i] = ++x;
-     }
+  cout << a[4] + a[5] * a[6] << endl;
+}
 
-     cout << a[4] + a[5] * a[6] << endl;
+// Discount program with ages
+void discountProgram() {
 
-     cout << endl;
+  int ages[5];
 
-     //Discount program with ages
-     int ages[5];
+  for (int i = 0; i < 5; ++i) {
+    cout << "Enter age: ";
+    cin >> ages[i];
+  }
 
-     for (int i = 0; i < 5; ++i)
-     {
-          cin >> ages[i];
-     }
+  float perc;
+  float min = ages[0];
 
-     float perc;
-     float min = ages[0];
+  for (int x = 1; x < 5; ++x) {
+    if (min > ages[x])
+      min = ages[x];
+  }
 
-     for (int x = 1; x < 5; ++x)
-     {
-          if (min > ages[x])
-               min = ages[x];
-     }
+  perc = 50 - (50 * min / 100);
+  cout << perc << endl;
+}
 
-     perc = 50 - (50 * min / 100);
-     cout << perc << endl;
+// Array loops concept
+void conceptArrLoops() {
+  int arrNum[9];
 
-     return 0;
+  cout << "Element - Value" << endl;
+
+  for (int i = 0; i <= 8; i++) {
+    arrNum[i] = 99;
+
+    cout << i << " --------- " << arrNum[i] << endl;
+  }
+}
+
+int main() {
+
+  listArr();
+
+  cout << endl;
+
+  discountProgram();
+
+  cout << endl;
+
+  conceptArrLoops();
+
+  return 0;
 }
