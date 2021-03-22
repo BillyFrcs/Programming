a1cdb5aacbf6364f66265fc8bd2beb166d7f0c00
Billy Franscois <69786531+BillyFrcs@users.noreply.github.com>
2021-03-22 23:18:18 +0900

    Update Include Header Problem Solving

--- Problem Solving/Input And Output/Src/Main.cpp
+++ Problem Solving/Input And Output/Src/Main.cpp
@@ -1,9 +1,4 @@
 #include <bits/stdc++.h>
-#include <iostream>
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
---
+++ Problem Solving/Pointer/Src/Main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void update(int *a, int *b) {
+  int firstPtr = *a, secondPtr = *b;
+
+  *a = (firstPtr + secondPtr);
+  *b = abs(firstPtr - secondPtr);
+}
+
+int main() {
+  int a, b;
+
+  int *result = &a, *result2 = &b;
+
+  cin >> a >> b;
+
+  update(result, result2);
+
+  cout << a << endl;
+  cout << b << endl;
+
+  return 0;
+}
