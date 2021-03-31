67f52d914b9071f2be55312cda902d91f180d1c0
Billy Franscois <69786531+BillyFrcs@users.noreply.github.com>
2021-03-31 22:48:44 +0900

    Update ASCII Program

--- Mix Program/Program/ASCII/Src/Main.cpp
+++ Mix Program/Program/ASCII/Src/Main.cpp
@@ -3,27 +3,42 @@
 
 using namespace std;
 
-void asciiProgram(){
+void asciiProgram()
+{
      char c1, c2, c3, c4, c5;
 
      cout << "Enter 5 leters: ";
      cin >> c1 >> c2 >> c3 >> c4 >> c5;
 
      cout << "ASCII: " << int(c1) << " " << int(c2) << " " << int(c3) << " "
-     << int(c4) << " " << int(c5) << endl;
+          << int(c4) << " " << int(c5) << endl;
 }
 
-void additionProgram(){
+void additionProgram()
+{
      int a = 20, b = 20;
      int c = ++a + ++b;
 
-     printf("%d", c);
+     printf("%d \n", c);
 }
 
+void name()
+{
+     string value = "6673767689", print;
+
+     size_t num = 0;
+
+     while (num < value.length())
+     {
+          print += char(10 * (int(value[num]) - 48) + (int(value[num + 1]) - 48));
+          num += 2;
+     }
+     cout << print << endl;
+}
 
 int main()
 {
-     //abbreviated American Standard Code for Information Interchange
+     //Abbreviated American Standard Code for Information Interchange
      //Concepts
      cout << (int)'a' << endl;
      cout << int('a') << endl;
@@ -37,4 +52,10 @@ int main()
      printf("\n");
 
      additionProgram();
+
+     printf("\n");
+
+     name();
+
+     return 0;
 }
