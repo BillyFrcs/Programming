43ac23d0e4dae604186ab74e7c5e659fd227e4f5
Billy Franscois <69786531+BillyFrcs@users.noreply.github.com>
2021-04-07 22:26:53 +0900

    New Update Linear Searching Algorithms

--- Data Structures And Algorithms/Algorithms/Searching/Linear Search/Lib/Main.cpp
+++ Data Structures And Algorithms/Algorithms/Searching/Linear Search/Lib/Main.cpp
@@ -2,45 +2,48 @@
 
 class linearSearch
 {
-private:
+public:
      int arr[5];
      int num;
-
-public:
-     //Input elements to search
-     void inputLinerSearch()
-     {
-          std::cout << "Enter 5 elements: ";
-
-          for (int i = 0; i < 5; i++)
-          {
-               std::cin >> arr[i];
-          }
-     }
-
-     //Search the elements
-     void LinearSearch()
-     {
-          int temp = -1;
-
-          std::cout << "Enter elements to search: ";
-          std::cin >> num;
-
-          for (int i = 0; i < 5; i++) //Less than total index array
-          {
-               if (arr[i] == num)
-               {
-                    //Using i + 1 to increase the array
-                    std::cout << "Found element at location: " << i + 1 << std::endl;
-                    temp = 0;
-               }
-          }
-          if (temp == -1)
-          {
-               std::cout << "Element not found" << std::endl;
-          }
-     }
+     
+     //Called the functions here
+     void inputLinerSearch();
+     void LinearSearch();
 };
+
+//Input elements to search
+void linearSearch::inputLinerSearch()
+{
+     std::cout << "Enter 5 elements: ";
+
+     for (int i = 0; i < 5; i++)
+     {
+          std::cin >> arr[i];
+     }
+}
+
+//Search the elements
+void linearSearch::LinearSearch()
+{
+     int temp = -1;
+
+     std::cout << "Enter elements to search: ";
+     std::cin >> num;
+
+     for (int i = 0; i < 5; i++) //Less than total index array
+     {
+          if (arr[i] == num)
+          {
+               //Using i + 1 to increase the array
+               std::cout << "Found element at location: " << i + 1 << std::endl;
+               temp = 0;
+          }
+     }
+     if (temp == -1)
+     {
+          std::cout << "Element not found" << std::endl;
+     }
+}
 
 int main(int argc, char const *argv[])
 {
