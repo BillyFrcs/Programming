83a7e140a81312536eef99bded174f516cc06b8a
Billy Franscois <69786531+BillyFrcs@users.noreply.github.com>
2021-04-09 12:22:27 +0900

    Update Loops Pattern Rectangle Shape

--- Flow Control/Loops/Pattern Rectangle Shape/Rectangle Shape/Lib/Main.cpp
+++ Flow Control/Loops/Pattern Rectangle Shape/Rectangle Shape/Lib/Main.cpp
@@ -4,6 +4,10 @@
 void patternNum()
 {
      int Num;
+     
+     std::ios_base::sync_with_stdio(false);
+     std::cin.tie(NULL);
+
      std::cin >> Num;
 
      for (int i = 1; i <= Num; i++)
@@ -26,9 +30,30 @@ void patternNum()
      getch();
 }
 
+void patternNumbers(int n)
+{
+
+     for (int i = 1; i <= n; i++)
+     {
+          for (int j = 1; j <= n; j++)
+          {
+               std::cout << j << " ";
+          }
+          std::cout << "\n";
+     }
+     getch();
+}
+
 int main(int, char **)
 {
+     //Pattern 1
      patternNum();
+
+     std::cout << "\n";
+
+     //Pattern 2
+     int num = 5;
+     patternNumbers(num);
 
      return 0;
 }
