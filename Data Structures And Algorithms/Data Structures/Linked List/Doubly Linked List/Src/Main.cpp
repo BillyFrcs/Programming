c84905e64a61e2c7325a398f094d3f158eea04d0
Billy Franscois <69786531+BillyFrcs@users.noreply.github.com>
2021-05-08 23:50:37 +0900

    New Add Doubly Linked List DS

--- Data Structures And Algorithms/Data Structures/Linked List/Doubly Linked List/Src/Main.cpp
+++ Data Structures And Algorithms/Data Structures/Linked List/Doubly Linked List/Src/Main.cpp
@@ -19,8 +19,8 @@ Node::Node()
 {
      key = 0;
      data = 0;
-     next = nullptr;
-     previous = nullptr;
+     next = NULL;
+     previous = NULL;
 }
 
 Node::Node(int k, int d)
@@ -37,7 +37,7 @@ public:
 
      DoublyLinkedList()
      {
-          head = nullptr;
+          head = NULL;
      }
 
      DoublyLinkedList(Node *n)
@@ -48,10 +48,10 @@ public:
      //Check te node if already exist by key value with return function
      Node *nodeExists(int k)
      {
-          Node *temp = nullptr;
+          Node *temp = NULL;
           Node *ptr = head;
 
-          while (ptr != nullptr)
+          while (ptr != NULL)
           {
                if (ptr->key == k)
                {
@@ -70,20 +70,30 @@ public:
 
      //Insert node
      void insertNode(int k, Node *n);
+
+     //Delete node
+     void deleteNode(int k);
+
+     //Update node
+     void updateNode(int k, int d);
+
+     //Print node
+     void printListNode();
 };
 
 //Append a node to the list
 void DoublyLinkedList::appendNode(Node *n)
 {
-     if (nodeExists(n->key) != nullptr)
+     if (nodeExists(n->key) != NULL)
      {
           std::cout << "Node already exists with key value: " << n->key << " append another node with different key value \n";
      }
 
      else
      {
-          if (head == nullptr)
+          if (head == NULL)
           {
+               head = n;
                std::cout << "Node appended by head node \n";
           }
 
@@ -91,9 +101,9 @@ void DoublyLinkedList::appendNode(Node *n)
           {
                Node *ptr = head;
 
-               while (ptr->next != nullptr)
+               while (ptr->next != NULL)
                {
-                    ptr = ptr->next;
+                    ptr = (ptr->next);
                }
 
                ptr->next = n;
@@ -106,14 +116,14 @@ void DoublyLinkedList::appendNode(Node *n)
 //Prepend the node at the first node
 void DoublyLinkedList::prependNode(Node *n)
 {
-     if (nodeExists(n->key) != nullptr)
+     if (nodeExists(n->key) != NULL)
      {
           std::cout << "Node already exists with key value: " << n->key << " append another node with different key value \n";
      }
 
      else
      {
-          if (head == nullptr)
+          if (head == NULL)
           {
                std::cout << "Node prepended by head node \n";
           }
@@ -133,26 +143,24 @@ void DoublyLinkedList::insertNode(int k, Node *n)
 {
      Node *ptr = nodeExists(k);
 
-     if (ptr == nullptr)
+     if (ptr == NULL)
      {
           std::cout << "No node exists by key value: " << k << "\n";
      }
 
      else
      {
-          if (nodeExists(n->key) != nullptr)
+          if (nodeExists(n->key) != NULL)
           {
                std::cout << "Node already exists with key value: " << n->key << " append another node with different key value \n";
           }
 
           else
           {
-               std::cout << "Inserting \n";
-
                Node *nextNode = ptr->next;
 
                //Inserting at the end node
-               if (nextNode == nullptr)
+               if (nextNode == NULL)
                {
                     ptr->next = n;
                     n->previous = ptr;
@@ -174,12 +182,171 @@ void DoublyLinkedList::insertNode(int k, Node *n)
      }
 }
 
+//Delete node by key value
+void DoublyLinkedList::deleteNode(int k)
+{
+     Node *ptr = nodeExists(k);
+
+     if (ptr == NULL)
+     {
+          std::cout << "No node exists with key value: " << k << "\n";
+     }
+     else
+     {
+          if (head == NULL)
+          {
+               std::cout << "Doubly linked list is empty, couldn't delete \n";
+          }
+          else if (head != NULL)
+          {
+               if (head->key == k)
+               {
+                    head = (head->next);
+                    std::cout << "Node unlinked with key value: " << k << "\n";
+               }
+               else
+               {
+                    Node *nextNode = (ptr->next);
+                    Node *prevNode = (ptr->previous);
+
+                    //Deleted at the end of node
+                    if (nextNode == NULL)
+                    {
+                         prevNode->next = nullptr;
+                         std::cout << "Node deleted at the end \n";
+                    }
+
+                    //Deleted in between the node
+                    else
+                    {
+                         prevNode->next = nextNode;
+                         nextNode->previous = prevNode;
+                         std::cout << "Node deleted in between \n";
+                    }
+               }
+          }
+     }
+}
+
+//Update the node
+void DoublyLinkedList::updateNode(int k, int d)
+{
+     Node *ptr = nodeExists(k);
+
+     if (ptr != NULL)
+     {
+          ptr->data = d;
+          std::cout << "Node data updated successfully \n";
+     }
+     else
+     {
+          std::cout << "Node doesn't exists with key value: " << k << "\n";
+     }
+}
+
+//Print the node
+void DoublyLinkedList::printListNode()
+{
+     if (head == NULL)
+     {
+          std::cout << "No node in doubly linked list \n";
+     }
+     else
+     {
+          std::cout << "\nDoubly linked list values: ";
+          Node *temp = head;
+
+          while (temp != NULL)
+          {
+               std::cout << "(" << temp->key << ", " << temp->data << ") <--> ";
+               temp = (temp->next);
+          }
+     }
+}
+
 void showDoublyLinkedList()
 {
+     DoublyLinkedList *DLL = new DoublyLinkedList();
+
+     int Options, key1, key2, data;
+
+     do
+     {
+          std::cout << "\nSelect option (0 to exit): \n";
+          std::cout << "1.) Append Node \n";
+          std::cout << "2.) Prepend Node \n";
+          std::cout << "3.) Insert Node \n";
+          std::cout << "4.) Delete Node \n";
+          std::cout << "5.) Update Node \n";
+          std::cout << "6.) Print Node \n";
+          std::cout << "7.) Clear console \n";
+
+          std::cout << "Choose option: ";
+          std::cin >> Options;
+
+          Node *n1 = new Node();
+
+          //Enter numbers
+          switch (Options)
+          {
+          case 1:
+               std::cout << "Enter key & data to append: ";
+               std::cin >> key1 >> data;
+               n1->key = key1;
+               n1->data = data;
+               DLL->appendNode(n1);
+               break;
+
+          case 2:
+               std::cout << "Enter key & data to prepend: ";
+               std::cin >> key1 >> data;
+               n1->key = key1;
+               n1->data = data;
+               DLL->prependNode(n1);
+               break;
+
+          case 3:
+               std::cout << "Enter existing key of node after insert the new node: ";
+               std::cin >> key2;
+
+               std::cout << "Enter key & data to the new first node: ";
+               std::cin >> key1 >> data;
+
+               n1->key = key1;
+               n1->data = data;
+               DLL->insertNode(key2, n1);
+               break;
+
+          case 4:
+               std::cout << "Enter key of the node to deleted: ";
+               std::cin >> key2;
+               DLL->deleteNode(key2);
+               break;
+
+          case 5:
+               std::cout << "Enter key & new data to update: ";
+               std::cin >> key1 >> data;
+               DLL->updateNode(key1, data);
+               break;
+
+          case 6:
+               DLL->printListNode();
+               break;
+
+          case 7:
+               system("cls");
+               break;
+
+          default:
+               std::cout << "Not found \n";
+               break;
+          }
+     } while (Options != 0);
 }
 
 int main()
 {
+     showDoublyLinkedList();
 
      return 0;
 }
