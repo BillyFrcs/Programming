f379cab62332c78be2ca5486c80b71ebe0ffdce1
Billy Franscois <69786531+BillyFrcs@users.noreply.github.com>
2021-05-08 21:53:27 +0900

    New Update Goto Flow Control

--- Flow Control/Goto/Src/Main.cpp
+++ Flow Control/Goto/Src/Main.cpp
@@ -1,9 +1,32 @@
-#include "Header.hpp"
 #include <iostream>
 
-using namespace std;
+void averageNumber()
+{
+     double num, average, sum = 0.0;
+     int i, n;
 
-void averageNumber();
+     std::cout << "Max of inputs = ";
+     std::cin >> n;
+
+     for (i = 1; i <= n; i++)
+     {
+          std::cout << "Enter num" << i << " = ";
+          std::cin >> num;
+
+          if (num <= 0.0)
+          {
+               goto statements;
+          }
+          sum += num;
+     }
+
+//Jump to this statements
+statements:
+
+     average = sum / (i - 1);
+
+     std::cout << "\nAverage = " << average << std::endl;
+}
 
 int main()
 {
