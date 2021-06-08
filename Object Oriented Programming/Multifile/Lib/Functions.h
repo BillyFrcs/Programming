2ebfd870c0435cbad16953489c0cea65d4651bfd
Billy Franscois <69786531+BillyFrcs@users.noreply.github.com>
2021-06-08 22:55:54 +0900

    New update MultiFile OOP

---
+++ Object Oriented Programming/Multifile/Lib/Functions.h
@@ -0,0 +1,29 @@
+#ifndef __DATADEVELOPER
+#define __DATADEVELOPER
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class dataDeveloper
+{
+public:
+     string name;
+     int age;
+
+     dataDeveloper(string name, int age)
+     {
+          this->name = name;
+          this->age = age;
+     }
+
+     void showDataDeveloper()
+     {
+          cout << "Name " << this->name << endl;
+          cout << "Age " << this->age << endl;
+     }
+};
+
+std::string sayHi(std::string name);
+
+#endif
---
+++ Object Oriented Programming/Multifile/Lib/Main.cpp
@@ -0,0 +1,16 @@
+#include "Functions.h"
+
+using namespace std;
+
+int main()
+{
+
+     dataDeveloper *dataDeveloper1 = new dataDeveloper("Billy", 18);
+
+     dataDeveloper1->showDataDeveloper();
+
+     cout << endl;
+
+     std::cout << sayHi("Billy");
+
+}
