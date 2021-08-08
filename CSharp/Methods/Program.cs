648f5ad48b469e16e5b2ea29b7ef37bec72e28b6
BillyFrcs <69786531+BillyFrcs@users.noreply.github.com>
2021-08-09 00:14:58 +0900

    New add Methods

---
+++ CSharp/Methods/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Methods
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               for (int i = 1; i < 5; i++)
+               {
+                    RandomNumber();
+               }
+
+               int loop = 0;
+               while (loop < 5)
+               {
+                    Alien();
+
+                    Console.WriteLine("\n");
+                    loop++;
+               }
+
+               int number1 = Convert.ToInt32(Console.ReadLine());
+               int number2 = Convert.ToInt32(Console.ReadLine());
+
+               if (Square(number1, number2) % 2 == 0)
+               {
+                    Console.Write(Square(number1, number2) + " is even number.\n");
+               }
+               else
+               {
+                    Console.Write(Square(number1, number2) + " is uneven number.\n");
+               }
+
+               Console.Write("Enter a sentence: ");
+               string sentence = Console.ReadLine();
+
+               CountNumberSentence(sentence);
+          }
+
+          static void RandomNumber()
+          {
+               Random rand = new Random();
+
+               int randNumber = rand.Next(0, 20);
+
+               Console.WriteLine(randNumber);
+          }
+
+          static void Alien()
+          {
+               Random randomAlien = new Random();
+
+               string alien = "B." + randomAlien.Next(0, 999999);
+               int age = randomAlien.Next(0, 1000);
+
+               Console.WriteLine("Hi i'm " + alien);
+               Console.WriteLine("I'm " + age + " years old");
+               Console.WriteLine("Oh, i'm an alien :)");
+          }
+
+          static int Square(int number1, int number2)
+          {
+               return number1 * number2;
+          }
+
+          static void CountNumberSentence(string sentence)
+          {
+               string[] words = sentence.Split(' ');
+
+               if (words.Length > 0)
+               {
+                    Console.WriteLine("There are " + words.Length + " words in that sentence.");
+               }
+          }
+     }
+}
