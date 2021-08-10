868289f9800edd56728a6962de717e9325453a44
BillyFrcs <69786531+BillyFrcs@users.noreply.github.com>
2021-08-10 21:32:10 +0900

    New add TypeCasting

---
+++ CSharp/TypeCasting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TypeCasting
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               // Implicit Casting
+               int myInt = 19;
+               double myDouble = myInt;
+
+               Console.WriteLine(myDouble);
+
+               // Explicit Casting
+               float myFloat = 1.4f;
+               int number = (int)myFloat;
+
+               Console.WriteLine(myFloat);
+               Console.WriteLine(number);
+
+               // Convertion any data types
+               int intNumber = 15;
+               double doubleNumber = 18.5D;
+               string isPlay = "false";
+
+               Console.WriteLine(Convert.ToString(intNumber));
+               Console.WriteLine(Convert.ToInt64(doubleNumber));
+               Console.WriteLine(Convert.ToBoolean(isPlay));
+          }
+     }
+}
