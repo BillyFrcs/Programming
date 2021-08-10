3803e9bba44484062a6b3a6868b8867c6fac87cd
BillyFrcs <69786531+BillyFrcs@users.noreply.github.com>
2021-08-10 15:09:55 +0900

    New add Python variables

---
+++ Python/Variables/Main.py
@@ -0,0 +1,13 @@
+# Set variables
+name = "Billy"
+number = 5
+decimalNumber = 19.0
+isTrue = True
+isFalse = False
+
+# Print the values of variables
+print("Name: " + name)
+print("Number:" + str(number))
+print("Decimal Number: " + str(decimalNumber))
+print("Is True: " + str(isTrue))
+print("Is False: " + str(isFalse))
