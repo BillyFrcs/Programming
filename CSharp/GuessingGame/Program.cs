93649daba8b1e92c47f248d21f01df619e583e6d
BillyFrcs <69786531+BillyFrcs@users.noreply.github.com>
2021-08-11 18:32:26 +0900

    New add GuessingGame

---
+++ CSharp/GuessingGame/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuessingGame
+{
+     class GuessingNumber
+     {
+          private int number;
+
+          public void GuessNumber()
+          {
+               do
+               {
+                    Console.Write("Guessing a number between 1 to 20: ");
+                    number = Convert.ToInt32(Console.ReadLine());
+
+                    if (number < 11)
+                    {
+                         Console.WriteLine("Too low!");
+                    }
+                    else if (number > 11)
+                    {
+                         Console.WriteLine("Too high!");
+                    }
+
+                    if (number == 11)
+                    {
+                         Console.WriteLine("You guess it, congrats...:D");
+                    }
+               } while (number != 11);
+          }
+     }
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               Console.ForegroundColor = ConsoleColor.Blue;
+
+               GuessingNumber guessingNumber = new GuessingNumber();
+
+               guessingNumber.GuessNumber();
+          }
+     }
+}
