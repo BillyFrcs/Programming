195e7ae5d0bcc893cf95064a6c8df07f30c3e790
BillyFrcs <69786531+BillyFrcs@users.noreply.github.com>
2021-08-14 21:05:46 +0900

    New add Collection

---
+++ Python/Colection/Main.py
@@ -0,0 +1,17 @@
+cars = ["Ferary", "Volvo", "BMW"]
+
+# Unpack the data
+a, b, c = cars
+print(a)
+print(b)
+print(c)
+
+# Basics mathematical operations
+x = 10
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
