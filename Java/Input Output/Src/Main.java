48b750ea508a40e469048e15b0fb8fcc8a458c2c
Billy Franscois <billyfranscois@outlook.com>
2021-12-11 19:07:59 +0900

    Update Input Output Java

--- Java/Input Output/Src/Main.java
+++ Java/Input Output/Src/Main.java
@@ -4,11 +4,21 @@ public class Main{
      public static void main(String args[]) throws Exception{
           Scanner scan = new Scanner(System.in);
 
-          System.out.print("Enter some numbers: ");
+          System.out.print("Enter some word: ");
 
           String input = scan.nextLine(); //The data type could change with another data types
+          System.out.println("Input is: " + input);
 
-          System.out.println("Result is: " + input);
+          System.out.print("Enter some number: ");
+
+          int inputNumber = scan.nextInt();
+          System.out.println("Number is: " + inputNumber);
+
+          float inputFloat = scan.nextFloat();
+          System.out.println("Float is: " + inputFloat);
+
+          double inputDouble = scan.nextDouble();
+          System.out.println("Double is: " + inputDouble);
 
           scan.close(); //Close the scanner to hide the warning 
      }
