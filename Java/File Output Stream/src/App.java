e8f15ecac0a3c2a73dcfa1887044f684a73ac5fd
Billy Franscois <billyfranscois@outlook.com>
2021-12-15 16:19:03 +0900

    Doing Formatting for File Output Stream

--- Java/File Output Stream/src/App.java
+++ Java/File Output Stream/src/App.java
@@ -1,54 +1,48 @@
 import java.io.*; // Import all the java.io packages
 
-public class App 
-{
-    public static void main(String[] args) throws IOException
-    {
-      // Method 1
-            // Create a fileoutputstream object (Buat folder dengan nama Test di C drive)
-            FileOutputStream FOS = new FileOutputStream("C://Test//Data.txt", true); // Lokasi file boleh dimodifikasi :)
-     
-            // Message to be written to the file
-            String message = "Yooooooooooooooooooooooooooooo";
-              
-            char character[] = message.toCharArray();
-            
-            /* For default
-            for (int i = 0; i < message.length(); i++) 
-            {
-                FOS.write(character[i]);
-            }
-            */
+public class App {
+  public static void main(String[] args) throws IOException {
+    // Method 1
+    // Create a fileoutputstream object (Buat folder dengan nama Test di C drive)
+    FileOutputStream FOS = new FileOutputStream("C://Test//Data.txt", true); // Lokasi file boleh dimodifikasi :)
 
-            // For each 
-            for(char item : character)
-            {
-              FOS.write(item);
-            }
-                 
-            FOS.close();
+    // Message to be written to the file
+    String message = "Yooooooooooooooooooooooooooooo";
 
-            // Method 2
-            SaveToFile("Heyoooooooooooooooooooooooooooooooooo", "YourFile");
+    char character[] = message.toCharArray();
+
+    /*
+     * For default
+     * for (int i = 0; i < message.length(); i++)
+     * {
+     * FOS.write(character[i]);
+     * }
+     */
+
+    // For each
+    for (char item : character) {
+      FOS.write(item);
     }
 
-    // Another implementation (using try & catch)
-    private static void SaveToFile(String message, String fileName)
-    {
-      try 
-      {
-        // Check filenya di dalam project kalian
-          FileOutputStream FileOutput = new FileOutputStream(fileName + ".txt");
+    FOS.close();
 
-          byte[] data = message.getBytes();
+    // Method 2
+    SaveToFile("Heyoooooooooooooooooooooooooooooooooo", "YourFile");
+  }
 
-          FileOutput.write(data);
+  // Another implementation (using try & catch)
+  private static void SaveToFile(String message, String fileName) {
+    try {
+      // Check filenya di dalam project kalian
+      FileOutputStream FileOutput = new FileOutputStream(fileName + ".txt");
 
-          FileOutput.close();
-      }
-      catch (Exception ErrorException) 
-      {
-          ErrorException.getStackTrace();
-      }
+      byte[] data = message.getBytes();
+
+      FileOutput.write(data);
+
+      FileOutput.close();
+    } catch (Exception ErrorException) {
+      ErrorException.getStackTrace();
     }
+  }
 }
