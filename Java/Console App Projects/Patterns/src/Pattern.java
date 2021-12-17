7d768d10ebc586ab52c059c8668ac0cb44bfb1db
Billy Franscois <billyfranscois@outlook.com>
2021-12-17 16:05:17 +0900

    Update Pattern Project

--- Java/Console App Projects/Patterns/src/Pattern.java
+++ Java/Console App Projects/Patterns/src/Pattern.java
@@ -1,35 +1,17 @@
 public class Pattern {
     public static void main(String[] args) throws Exception {
 
-        // Pattern.Pattern1(20);
+        Pattern.Pattern1(1, 20);
 
-        Pattern.Pattern2(1, 20);
+        // Pattern.Pattern2(20);
 
-        // Pattern.Pattern3(20);
+        Pattern.Pattern3(30);
     }
 
-    public static void Pattern1(int number) {
-        StringBuilder Builder = new StringBuilder();
-
-        int i = 1;
-
-        do {
-            Builder.append(i + " ");
-
-            if (i < 10) {
-                Builder.append("\n");
-            }
-
-        } while (i++ < number);
-
-        System.out.println(Builder.toString());
-    }
-
-    private static void Pattern2(int iteration, int number) {
+    private static void Pattern1(int iteration, int number) {
         StringBuilder Builder = new StringBuilder();
 
         do {
-            // System.out.println(iteration);
             Builder.append(iteration + " ");
 
             if (iteration <= 10) {
@@ -41,7 +23,7 @@ public class Pattern {
         System.out.println(Builder.toString());
     }
 
-    private static void Pattern3(int number) {
+    private static void Pattern2(int number) {
         StringBuilder Builder = new StringBuilder();
 
         for (int i = 1; i < number; i++) {
@@ -54,5 +36,19 @@ public class Pattern {
         }
 
         System.out.println(Builder.toString());
+    }
+
+    private static void Pattern3(int i) {
+        do {
+            System.out.print(i + " ");
+            i--;
+        } while (i >= 16);
+
+        int j = 1;
+
+        do {
+            System.out.print(j + " ");
+            j++;
+        } while (j <= 15);
     }
 }
