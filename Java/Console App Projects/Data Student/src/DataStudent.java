4546fce46a7ddb26c81337accfb32b0e570c1046
Billy Franscois <billyfranscois@outlook.com>
2021-12-18 22:14:36 +0900

    Update DataStudent.java

--- Java/Console App Projects/Data Student/src/DataStudent.java
+++ Java/Console App Projects/Data Student/src/DataStudent.java
@@ -4,8 +4,6 @@ public class DataStudent {
     private String _name;
     private long _NPM;
     private String _address;
-
-    // private long _phoneNumber;
     private String _phoneNumber;
     private String _email;
 
@@ -26,14 +24,12 @@ public class DataStudent {
     }
 
     private void SetNameStudent() {
-        // Input name
         System.out.print("Masukan Nama\t: ");
 
         _name = _ScanData.nextLine();
     }
 
     private void SetNPMStudent() {
-        // Input NPM
         System.out.print("Masukan NPM\t: ");
 
         try {
@@ -46,30 +42,24 @@ public class DataStudent {
     }
 
     private void SetAddressStudent() {
-        // Input address
         System.out.print("Masukan Alamat\t: ");
 
         _address = _ScanData.nextLine();
     }
 
     private void SetPhoneNumberStudent() {
-        // Input phone number
         System.out.print("Masukan No.Telp\t: ");
-
-        // _phoneNumber = Long.parseLong(_ScanData.nextLine());
 
         _phoneNumber = _ScanData.nextLine();
     }
 
     private void SetEmailStudent() {
-        // Input email
         System.out.print("Masukan Email\t: ");
 
         _email = _ScanData.nextLine();
     }
 
     private void GetDataStudent() {
-        // Print all this shit
         System.out.println("Nama\t: " + _name);
         System.out.println("NPM\t: " + _NPM);
         System.out.println("Alamat\t: " + _address);
