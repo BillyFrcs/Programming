98e7132d3b6314237615a5bb8bcd49ff6f5c5691
Billy Franscois <billyfranscois@outlook.com>
2022-01-19 00:59:00 +0900

    Adding New Language Visual Basic .NET

---
+++ VB.NET/DataTypes/Program.vb
@@ -0,0 +1,18 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        ' Boolean 
+        Dim isStart As Boolean = True
+        Console.WriteLine($"Value {isStart}")
+
+        ' Byte
+        Dim number As Byte = 19
+        Console.WriteLine($"Number: {number}")
+
+        ' String
+        Dim name As String = "Billy"
+        Console.WriteLine($"Name: {name}")
+
+    End Sub
+End Module
