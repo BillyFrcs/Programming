d22dd92ef5214f8380e9e1d299d681aa7aa031c2
Billy Franscois <billyfranscois@outlook.com>
2022-01-19 10:17:31 +0900

    Adding new Data Types

--- Visual Basic .NET/DataTypes/Program.vb
+++ Visual Basic .NET/DataTypes/Program.vb
@@ -10,9 +10,19 @@ Module Program
         Dim number As Byte = 19
         Console.WriteLine($"Number: {number}")
 
+        Dim decimalNumber As Decimal = 4.5F
+        Console.WriteLine($"Decimal {decimalNumber}")
+
+        ' Double
+        Dim doubleNumber As Double = 10.54D
+        Console.WriteLine($"Decimal number {doubleNumber}")
+
         ' String
         Dim name As String = "Billy"
-        Console.WriteLine($"Name: {name}")
+        Console.WriteLine($"Name: {name.ToUpper}")
 
+        ' Char
+        Dim alphabet As Char = "B"
+        Console.WriteLine($"Alphabet {alphabet}")
     End Sub
 End Module
