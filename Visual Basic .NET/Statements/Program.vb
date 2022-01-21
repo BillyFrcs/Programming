afa9a43e89b5a50cb0e10554551705c8a2257158
Billy Franscois <billyfranscois@outlook.com>
2022-01-22 00:12:06 +0900

    Added Selected Case

--- Visual Basic .NET/Statements/Program.vb
+++ Visual Basic .NET/Statements/Program.vb
@@ -13,5 +13,17 @@ Module Program
         Else
             Console.WriteLine($"Value of {value} is negative.")
         End If
+
+        ' Select case
+        Dim weapon = "M4"
+
+        Select Case weapon
+            Case "M4"
+                Console.WriteLine($"Player use {weapon}")
+            Case "AK47"
+                Console.WriteLine($"Player use {weapon}")
+            Case Else
+                Console.WriteLine("Player didn't use weapon")
+        End Select
     End Sub
 End Module
