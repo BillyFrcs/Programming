41ca0785492ccf73ecf1d50cb60437e908aff4a7
Billy Franscois <BillyFranscois19@outlook.com>
2022-04-16 19:06:49 +0900

    Added String

---
+++ CSharp/String/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace String
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               string[] words = {
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+               Console.Write("Enter a letter: ");
+               string letter = Console.ReadLine();
+
+               int count = 0;
+
+               foreach (string word in words)
+               {
+                    if (word.Contains(letter))
+                    {
+                         Console.WriteLine(word);
+
+                         count++;
+                    }
+               }
+
+               if (count == 0)
+               {
+                    Console.WriteLine("No match");
+               }
+          }
+     }
+}
