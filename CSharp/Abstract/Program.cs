20878657895f73466ac59544e2083aa1d9c44170
Billy Franscois <BillyFranscois@outlook.com>
2022-04-19 19:38:31 +0900

    Added Abstract

---
+++ CSharp/Abstract/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Abstract
+{
+     // Abstarct Book Class
+     abstract class Book
+     {
+
+          // Private Fields
+          private string _name;
+          private string _author;
+          private string _price;
+
+          protected string Name
+          {
+               get { return this._name; }
+          }
+          protected string Author
+          {
+               get { return this._author; }
+          }
+          protected string Price
+          {
+               get { return this._price; }
+          }
+
+          // Parameterized Constructor
+          public Book(string name, string author, string price)
+          {
+               this._name = name;
+               this._author = author;
+               this._price = price;
+          }
+
+          // Abstract Method
+          public abstract string GetDetails();
+
+     }
+
+     // Class MyBook extending Book Class
+     class MyBook : Book
+     {
+
+          // Parameterized Constructor
+          public MyBook(string name, string author, string price)
+            : base(name, author, price)
+          { }
+
+          // Overrideing the GetDetails Abstract Method of the Base Class
+          public override string GetDetails()
+          {
+               return Name + ", " + Author + ", " + Price;
+          }
+
+     }
+
+     class Demo
+     {
+
+          public static void Main(string[] args)
+          {
+               Book myBook = new MyBook("Harry Potter", "J.k. Rowling", "100");
+
+               Console.WriteLine(myBook.GetDetails());
+          }
+     }
+}
