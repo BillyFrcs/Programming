32230e126a62b5264bff2213f837ef7e9cd6e28b
Billy Franscois <BillyFranscois@outlook.com>
2022-04-21 01:31:10 +0900

    Added Object Destructuring

---
+++ Javascript/ObjectDestructuring/app.js
@@ -0,0 +1,14 @@
+const data =
+{
+     name: "Billy",
+     age: 18,
+     ID: 190820
+};
+
+const name = data.name;
+const age = data.age;
+const ID = data.ID;
+
+console.log(`Name: ${name}`);
+console.log('Age: ' + age);
+console.log('ID: ' + ID);
