4b4ad4682eeca259cb22d868702cad487ea60f4f
Billy Franscois <BillyFranscois@outlook.com>
2022-04-20 16:36:05 +0900

    Adding simple input

---
+++ Javascript/VariableDeclarations/app.js
@@ -0,0 +1,13 @@
+const readline = require("readline");
+
+const input = readline.createInterface({
+     input: process.stdin,
+     output: process.stdout
+});
+
+input.question("Enter your name: ", function (name)
+{
+     console.log("Hello " + name);
+
+     input.close();
+});
