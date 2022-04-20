8aa3bbd087a1475d0a8036b0f051378fcf9a3d16
Billy Franscois <BillyFranscois@outlook.com>
2022-04-20 21:37:13 +0900

    Added Binary Converter

---
+++ CSharp/Converter/Binary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Converter.Binary
+{
+     public class Converter
+     {
+          public static String ToBinary(int num)
+          {
+               String binary = "";
+
+               while (num > 0)
+               {
+                    binary = (num % 2) + binary;
+
+                    num /= 2;
+               }
+
+               return binary;
+          }
+     }
+
+     public class Program
+     {
+          private static void Main(string[] args)
+          {
+               Console.Write("Input number: ");
+               int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+               Console.WriteLine(Converter.ToBinary(inputNumber));
+          }
+     }
+}
