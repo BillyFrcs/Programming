b8a94730f762d95f75f72c3ed8c441255ade4387
Billy Franscois <BillyFranscois@outlook.com>
2022-04-22 15:14:13 +0900

    Update Main.py

--- Python/Loops/For/Main.py
+++ Python/Loops/For/Main.py
@@ -2,3 +2,13 @@ number = 5
 
 for i in range(100):
     print(i * number)
+
+print('\n')
+
+name = ['Billy', 'Sendy', 'John', 'Bob', 'Mary', 'Jane', 'Jack']
+
+for n in name:
+    print("Name: " + n)
+
+for i in range(10):
+    print("Number-" + str(i + 1))  # Start from 1
