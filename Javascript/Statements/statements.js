0811eae0be8bf106eb6e908fd145f171e6fe83b2
Billy Franscois <BillyFranscois@outlook.com>
2022-04-22 14:36:12 +0900

    Adding if statements

---
+++ Javascript/Statements/statements.js
@@ -0,0 +1,9 @@
+let movementSpeed = 100
+let canMove = true;
+
+if (movementSpeed > 0 && canMove === true) {
+     console.log("Moving forward at speed " + movementSpeed);
+}
+else {
+     console.log("Stopping");
+}
