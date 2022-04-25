e5751186b1f77177c06167056c17883a6e9722ba
Billy Franscois <BillyFranscois@outlook.com>
2022-04-25 22:56:10 +0900

    Added switch case

---
+++ Javascript/Statements/main.js
@@ -0,0 +1,19 @@
+let weapon = "M4"
+
+switch (weapon) {
+     case "M4":
+          console.log("You have a M4");
+          break;
+
+     case "AK47":
+          console.log("You have an AK47");
+          break;
+
+     case "AWP":
+          console.log("You have an AWP");
+          break;
+
+     default:
+          console.log("You have nothing!");
+          break;
+}
