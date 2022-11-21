89991b7f42f6c67bfdd3d8c35a492af5baca1809
Billy Franscois <BillyFranscois@outlook.com>
2022-11-21 22:27:14 +0900

    Update test.js

--- Javascript/Statements/test.js
+++ Javascript/Statements/test.js
@@ -35,3 +35,16 @@ try {
 } catch(e) {
     console.log("Out of bounds");
 }
+
+function fetchUsername() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('JSUser');
+            }, 3000);
+    })
+}
+
+console.log("Fetching username...");
+const username = fetchUsername();
+console.log(`You are logged in as ${username}`);
+console.log("Welcome!");
