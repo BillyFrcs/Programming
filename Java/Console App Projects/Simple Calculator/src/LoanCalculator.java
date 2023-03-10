c3a86b7d759f652b0b17675ac31f48f0d7ce40e4
Billy Franscois <BillyFranscois@outlook.com>
2023-03-10 23:27:11 +0900

    Adding input message

--- Java/Console App Projects/Simple Calculator/src/LoanCalculator.java
+++ Java/Console App Projects/Simple Calculator/src/LoanCalculator.java
@@ -5,6 +5,8 @@ public class LoanCalculator
 	public static void main(String[] args) {
 		Scanner Scan = new Scanner(System.in);
 
+		System.out.print("Enter the amount: ");
+
 		int amount = Scan.nextInt();
 
 		for (int i = 0; i < 3; i++)
