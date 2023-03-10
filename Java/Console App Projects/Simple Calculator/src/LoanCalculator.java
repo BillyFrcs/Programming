f831d45f835ee907ab8f890e004af06ae0982aad
Billy Franscois <BillyFranscois@outlook.com>
2023-03-10 23:15:10 +0900

    Adding Loan Calculator

---
+++ Java/Console App Projects/Simple Calculator/src/LoanCalculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LoanCalculator
+{
+	public static void main(String[] args) {
+		Scanner Scan = new Scanner(System.in);
+
+		int amount = Scan.nextInt();
+
+		for (int i = 0; i < 3; i++)
+        {
+            amount -= amount * 0.1;
+        }
+        
+        Scan.close();
+
+		System.out.println(amount);
+	}
+}
