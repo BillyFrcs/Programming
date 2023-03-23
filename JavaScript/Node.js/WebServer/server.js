1d0eea764e01316a024bb1e842970860f6b08d21
Billy Franscois <BillyFranscois@outlook.com>
2023-03-24 00:44:28 +0900

    Adding Routing Request

--- JavaScript/Node.js/WebServer/server.js
+++ JavaScript/Node.js/WebServer/server.js
@@ -1,11 +1,63 @@
 const http = require('http');
 
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/HTML');
+    response.setHeader('Content-Type', 'text/html');
 
     response.statusCode = 200;
 
-    response.end('<h1>Hello World!</h1>');
+    const { method, url } = request;
+
+    if (url === '/') {
+        if (method === 'GET') {
+            response.end('<h1>This is the home page</h1>');
+        } else {
+            response.end(`<h1>Cannot access page with ${method} request!</h1>`);
+        }
+    } else if (url === '/about') {
+        if (method === 'GET') {
+            response.end('<h1>This is the about page</h1>');
+        } else if (method === 'POST') {
+            let body = [];
+
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+
+                const { name } = JSON.parse(body);
+
+                response.end(`<h1>Hello ${name}!, This the about page</h1>`);
+            });
+        } else {
+            response.end(`<h1>Cannot access page with ${method} request!</h1>`);
+        }
+    } else {
+        response.end('<h1>Page not found!</h1>');
+    }
+
+    /*
+    if (method === 'GET') {
+        response.end('<h1>Hello Billy!</h1>');
+    }
+
+    if (method === 'POST') {
+        let body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+
+            const { name } = JSON.parse(body);
+            
+            response.end(`<h1>Hello ${name}!</h1>`);
+        });
+    }
+    */
 }
 
 const server = http.createServer(requestListener);
