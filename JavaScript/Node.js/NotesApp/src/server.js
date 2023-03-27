e824721d9be39417c7b1b6eeb37cc1e24d2ade74
Billy Franscois <BillyFranscois@outlook.com>
2023-03-27 18:43:10 +0900

    Change host

--- JavaScript/Node.js/NotesApp/src/server.js
+++ JavaScript/Node.js/NotesApp/src/server.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 1908,
+    port: 5000,
     host: 'localhost',
     routes: {
       cors: {
