1f4d25c5dd9f5d7bb9c7964d7994565ccad2b58c
Billy Franscois <BillyFranscois@outlook.com>
2024-06-27 00:11:32 +0900

    Adding Coin Flip logic

---
+++ CSharp/CoinFlip/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int coin = rand.Next(0, 2);
+
+            Console.Write("Coin flip result: ");
+
+            Console.WriteLine((coin == 0) ? "Heads" : "Tails");
+        }
+    }
+}
